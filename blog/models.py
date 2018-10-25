9a4a57bd9647b019bf485e8f1aff41e8506263ed
unknown <2120343354@qq.com>
2018-10-25 16:59:31 +0800

    step8: blog detail view --20181025

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 
 class Category(models.Model):
@@ -67,3 +68,8 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    # 自定义 get_absolute_url 方法
+    # 记得从 django.urls 中导入 reverse 函数
+    def get_absolute_url(self):
+        return reverse('blog:detail', kwargs={'pk': self.pk})
